4d17af90ecd8908ced830a2086231c9693b3cf4d
spelldone <spelldone1@gmail.com>
2021-03-15 16:41:07 +0400

    feat: add realization if check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  const st = [] ,openBrack = [], closeBrack = []
+  bracketsConfig.forEach((i) => {
+    openBrack.push(i[0])
+    closeBrack.push(i[1])
+  })
+
+  for ( let k = 0; k < str.length; k++) {
+    if(closeBrack.includes(str[k])&&st.length>0){
+      if(st[st.length-1]==openBrack[closeBrack.indexOf(str[k])])
+      st.pop()
+      else if(str[k]==openBrack[closeBrack.indexOf(str[k])])
+      st.push(str[k])
+      else return false
+    }
+    else st.push(str[k])
+}
+
+ return st.length==0
+ 
 }
